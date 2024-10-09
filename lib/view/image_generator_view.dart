6bd7d9d878240ba9ffa74cba7921f5adabe32f27
Haider228 <flutter2830@gmail.com>
2024-10-09 14:03:59 +0500

    adding the image geenrator file for UI

---
+++ lib/view/image_generator_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ImageGeneratorView extends StatefulWidget {
+  const ImageGeneratorView({super.key});
+
+  @override
+  State<ImageGeneratorView> createState() => _ImageGeneratorViewState();
+}
+
+class _ImageGeneratorViewState extends State<ImageGeneratorView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
