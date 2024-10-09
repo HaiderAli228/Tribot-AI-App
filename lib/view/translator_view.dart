ef3620a02c54af7a99ab40c8ef5780c3d5925743
Haider228 <flutter2830@gmail.com>
2024-10-09 14:06:57 +0500

    adding the chat and translator view

---
+++ lib/view/translator_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class TranslatorView extends StatefulWidget {
+  const TranslatorView({super.key});
+
+  @override
+  State<TranslatorView> createState() => _TranslatorViewState();
+}
+
+class _TranslatorViewState extends State<TranslatorView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
