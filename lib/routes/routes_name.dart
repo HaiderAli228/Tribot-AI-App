c30fc39b6d45a59581a378957cc92ee76f9abc69
Haider228 <flutter2830@gmail.com>
2024-10-10 19:01:22 +0500

    adding routes name

--- lib/routes/routes_name.dart
+++ lib/routes/routes_name.dart
@@ -1,4 +1,7 @@
 class RoutesName{
   static const String homeScreen = "home_screen" ;
   static const String splashScreen = "splash_screen" ;
+  static const String chatScreen = "chat_screen" ;
+  static const String imageGeneratorScreen = "image_generator_screen" ;
+  static const String translatorScreen = "translator_screen" ;
 }
